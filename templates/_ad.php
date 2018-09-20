01f649252563ee3451b604718ebf46f82fcf6855
Elena Petrenko <k-i-s-u-n-j-a@list.ru>
2018-09-20 23:16:34 +0300

    Добавлена фильтрация данных от пользователя

--- templates/_ad.php
+++ templates/_ad.php
@@ -1,14 +1,14 @@
 <li class="lots__item lot">
     <div class="lot__image">
-        <img src="<?=$ad['url']; ?>" width="350" height="260" alt="<?=$ad['title']; ?>">
+        <img src="<?= $ad['url']; ?>" width="350" height="260" alt="<?= htmlspecialchars($ad['title']); ?>">
     </div>
     <div class="lot__info">
         <span class="lot__category"><?=$ad['category']; ?></span>
-        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad['title']; ?></a></h3>
+        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($ad['title']); ?></a></h3>
         <div class="lot__state">
             <div class="lot__rate">
-                <span class="lot__amount"><?=$ad['price']; ?></span>
-                <span class="lot__cost"><?=format_price($ad['price']); ?></span>
+                <span class="lot__amount"><?= htmlspecialchars($ad['price']); ?></span>
+                <span class="lot__cost"><?= format_price(htmlspecialchars($ad['price'])); ?></span>
             </div>
             <div class="lot__timer timer">
 
