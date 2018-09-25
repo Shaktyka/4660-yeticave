885f43074ebdf0a709b12f17bbd56c2b36c9fba5
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-09-25 23:20:11 +0500

    Добавлены запросы INSERT на вставку категорий и пользователей

---
+++ queries.sql
@@ -0,0 +1,28 @@
+/* Вставка списка категорий */
+
+INSERT INTO categories (category)
+VALUES ('Доски и лыжи');
+INSERT INTO categories (category)
+VALUES ('Крепления');
+INSERT INTO categories (category)
+VALUES ('Ботинки');
+INSERT INTO categories (category)
+VALUES ('Одежда');
+INSERT INTO categories (category)
+VALUES ('Инструменты');
+INSERT INTO categories (category)
+VALUES ('Разное');
+
+/* Вставка данных двух пользователей */
+
+INSERT INTO users (email, user_pass, user_name, contacts, avatar, reg_date)
+VALUES ('ignat.v@gmail.com', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', 'Игнат', 'Skype: ignat_boss', 'img/avatar.jpg', '2018-09-24 10:20:05');
+
+INSERT INTO users (email, user_pass, user_name, contacts, avatar, reg_date)
+VALUES ('kitty_93@li.ru', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', 'Леночка', 'ВКонтакте https://vk.com/lenochka', 'img/lena.jpg', '2018-09-25 12:55:55');
+
+/* Вставка данных по объявлениям */
+
+
+/* */
+
