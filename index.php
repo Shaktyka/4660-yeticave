150082f39af17c91678ac632aaf6187ea4c4f521
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-09-28 22:49:33 +0500

    Добавлен запрос списка категорий из БД

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php require('functions.php'); 
+<?php
+$connect = mysqli_connect('localhost', 'root', '', 'yeticave');
+mysqli_set_charset($connect, 'utf8');
+ 
+if (!$connect){
+    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+} else {
+    // Запрашиваем список категорий из базы
+    $query = 'SELECT * FROM categories';
+    $result = mysqli_query($connect, $query);
+    
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+require('functions.php'); 
 
 require ('data.php');
 
