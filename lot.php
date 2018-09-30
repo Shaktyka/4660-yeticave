e089efd20245eb4a0f3c251b816564b9412b2ba3
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-10-01 01:08:47 +0500

    Рефакторинг скрипта для вывода статуса 404

--- lot.php
+++ lot.php
@@ -6,10 +6,6 @@ require_once('data.php');
 $connect = mysqli_connect("localhost", "root", "", "yeticave");
 mysqli_set_charset($connect, "utf8");
 
-if (isset($_GET['id'])) {
-    $lot_id = $_GET['id'];
-}
-
 if (!$connect){
     
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
@@ -17,29 +13,43 @@ if (!$connect){
     
 } else {
     
-    $query = 'SELECT l.lot_id, title, category, description, img_path, start_date, end_date, start_price, bet_step, MAX(amount) AS cur_price 
-    FROM lots l
-    JOIN categories c
-    ON l.category_id = c.cat_id
-    JOIN bets b
-    ON l.lot_id = b.lot_id
-    WHERE l.lot_id =' . $lot_id;
+    if (isset($_GET['id']) && $_GET['id'] == true) {
     
-    $result = mysqli_query($connect, $query);
-    
-    if (!$result) {
-        $error = mysqli_error($connect);
-        print("Ошибка MySQL: " . $error);
-        die();
-    } else {
+        $lot_id = intval($_GET['id']);
         
-        $ad = mysqli_fetch_assoc($result);
-        //print_r($categories);
-    }
+        $query = 'SELECT l.lot_id, title, category, description, img_path, start_date, end_date, start_price, bet_step, MAX(amount) AS cur_price 
+        FROM lots l
+        JOIN categories c
+        ON l.category_id = c.cat_id
+        JOIN bets b
+        ON l.lot_id = b.lot_id
+        WHERE l.lot_id =' . $lot_id;
     
-}
-
-$page_content = include_template('lot.php', ['categories' => $categories, 'ad' => $ad]);
+        $result = mysqli_query($connect, $query);
+    
+        if (!$result) {
+        
+            $error = mysqli_error($connect);
+            print("Ошибка MySQL: " . $error);
+            die();
+        
+            } else if (!$result) {
+            
+                $page_content = 'Ошибка 404: страница не найдена. Попробуйте уточнить параметры запроса.';
+            
+            } else {
+        
+                $ad = mysqli_fetch_assoc($result);
+                $page_content = include_template('lot.php', ['categories' => $categories, 'ad' => $ad]);
+            }
+    
+        } else {
+    
+            $page_content = 'Ошибка 404: страница не найдена. Попробуйте уточнить параметры запроса.';
+    
+        }
+    
+    }
 
 $layout_content = include_template('layout.php', ['content' => $page_content, 'user_name' => $user_name, 'title' => $title, 'categories' => $categories]);
 
