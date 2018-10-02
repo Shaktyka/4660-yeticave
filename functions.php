47cccf48990b5ee4b611bbda84e9a6674af4243b
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-10-02 13:01:50 +0500

    Добавлена функция для запроса отдельного лота по его id

--- functions.php
+++ functions.php
@@ -73,6 +73,49 @@ function get_open_lots($connect) {
 };
 
 
+// Получение данных лота по его id
+function get_lot_data($connect, $lot_id) { 
+    
+    if (isset($lot_id)) {
+        
+        $query = 'SELECT l.lot_id, title, category, description, img_path, start_date, end_date, start_price, bet_step, MAX(amount) AS cur_price 
+        FROM lots l
+        JOIN categories c
+        ON l.category_id = c.cat_id
+        JOIN bets b
+        ON l.lot_id = b.lot_id
+        WHERE l.lot_id =' . $lot_id;
+    
+        $result = mysqli_query($connect, $query);
+        $res_count = mysqli_num_rows($result);
+        //print_r($res_count);
+        
+        if ($res_count < 1) {
+            http_response_code(404);
+            die();
+        }
+    
+        if (!$result) {
+        
+            $error = mysqli_error($connect);
+            print("Ошибка MySQL: " . $error);
+            die();
+        
+        } else {
+        
+            $ad = mysqli_fetch_assoc($result);
+            return $ad;
+    
+        }
+    
+    } else {
+        http_response_code(404);
+        die();
+    }
+    
+};
+
+
 // Подключение шаблонов
 function include_template($name, $data) {
     $name = 'templates/' . $name;
--- lot.php
+++ lot.php
@@ -5,54 +5,19 @@ require_once('data.php');
 
 $connect = db_connect();
 
-$lot_id = (int)($_GET['id']);
-
 if ($connect) {
     
     $categories = get_categories($connect);
     
-    if (isset($lot_id)) {
+    $lot_id = (int)($_GET['id']); // как вариант, intval
+    $ad = get_lot_data($connect, $lot_id);
     
-        $lot_id = intval($_GET['id']);
-        
-        $query = 'SELECT l.lot_id, title, category, description, img_path, start_date, end_date, start_price, bet_step, MAX(amount) AS cur_price 
-        FROM lots l
-        JOIN categories c
-        ON l.category_id = c.cat_id
-        JOIN bets b
-        ON l.lot_id = b.lot_id
-        WHERE l.lot_id =' . $lot_id;
-    
-        $result = mysqli_query($connect, $query);
-        $res_count = mysqli_num_rows($result);
-        //print_r($res_count);
-        
-        if ($res_count < 1) {
-            http_response_code(404);
-            die();
-        }
-    
-        if (!$result) {
-        
-            $error = mysqli_error($connect);
-            print("Ошибка MySQL: " . $error);
-            die();
-        
-            } else {
-        
-                $ad = mysqli_fetch_assoc($result);
-                $page_content = include_template('lot.php', ['categories' => $categories, 'ad' => $ad]);
+    $page_content = include_template('lot.php', ['categories' => $categories, 'ad' => $ad]);
             
-                $layout_content = include_template('layout.php', ['content' => $page_content, 'user_name' => $user_name, 'title' => $title, 'categories' => $categories]);
+    $layout_content = include_template('layout.php', ['content' => $page_content, 'user_name' => $user_name, 'title' => $title, 'categories' => $categories]);
 
-                print($layout_content);
-            }
+    print($layout_content);
     
-        } else {
-            http_response_code(404);
-            die();
-        }
-    
-    }
+}
 
 ?>
