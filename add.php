c757e3e23144f33e9b09959c3fdbc6271aa4bb16
Shaktyka <k-i-s-u-n-j-a@list.ru>
2018-10-08 23:48:18 +0500

    Правки валидации полей формы по ТЗ

--- add.php
+++ add.php
@@ -12,7 +12,7 @@ if ($connect) {
 if ($_SERVER['REQUEST_METHOD' == 'POST']) {
     
     $advert = $_POST; // массив значений полей
-    $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date']; // обязательные поля
+    $required_fields = ['lot-name', 'category', 'message', 'file', 'lot-rate', 'lot-step', 'lot-date']; // обязательные поля
     $dict = ['lot-name' => 'Название', 'category' => 'Категория', 'message' => 'Описание', 'file' => 'Изображение', 'lot-rate' => 'Начальная цена', 'lot-step' => 'Шаг ставки', 'lot-date' => 'Дата завершения'];
     $errors = [];
     
@@ -39,30 +39,42 @@ if ($_SERVER['REQUEST_METHOD' == 'POST']) {
         $errors[$key] = 'Введённое значение должно быть числом';
     }
     
+    if ($start_price < 0) {
+        $errors[$key] = 'Введённое значение должно быть больше нуля';
+    }
+    
     // Проверяем значение числовых полей, что они числа
     if (!filter_var($bet_step, FILTER_VALIDATE_INT)) {
         $errors[$key] = 'Введённое значение должно быть числом';
     }
     
+    if (!is_int($bet_step)) {
+        $errors[$key] = 'Введённое значение должно быть целым числом';
+    }
+    
+    if ($bet_step < 0) {
+        $errors[$key] = 'Введённое значение должно быть больше нуля';
+    }
     
     // Проверяем, был ли загружен файл
     if (isset($_FILES['file']['name'])) {
 		$tmp_name = $_FILES['file']['tmp_name'];
-		$path = $_FILES['file']['name'];
+        $tmp_size = $_FILES['file']['size'];
+		$path = $_FILES['file']['name']; // похоже, ошибка
         
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $file_type = finfo_file($finfo, $tmp_name);
         
-        if ($file_type !== "image/jpeg" || $file_type !== "image/png") {
-            $errors['file'] = 'Загрузите изображение в формате JPEG или PNG';
+        if ($file_type !== "image/jpeg" || $file_type !== "image/jpg" || $file_type !== "image/png") {
+            $errors['file'] = 'Загрузите изображение в формате jpg, jpeg или png';
         } else {
             move_uploaded_file($tmp_name, 'img/' . $path);
-			$$advert['path'] = $path;
+			$advert['path'] = $path;
         }
 	}
     
     // Если есть ошибки, то отображаем их
-    if (count($errors) > 0) {
+    if (count($errors)) {
         // в тег формы добавить класс form--invalid
         // для всех полей формы, где найдены ошибки:
         // добавить контейнеру с этим полем класс form__item--invalid;
